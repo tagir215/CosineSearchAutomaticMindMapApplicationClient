b8c58db58692f8fc6db80facc3cf3e6df717ca16
Tiger <tagir.tsarkov@gmail.com>
2023-04-26 16:49:37 +0300

    hmm

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -5,7 +5,6 @@ import Tree from './Tree.js';
 export default function InputField(){
     const  [ values ,setValues ] = useState([]);
     const [text,setText] = useState("");
-    const [initState , setInitState ] = useState("starting...");
     const baseURL = "https://csamma.herokuapp.com";
     const handleChange = function(event){
         setText(event.target.value);
@@ -37,7 +36,6 @@ export default function InputField(){
             <input className="input-field" type="text" onChange={handleChange} onKeyUp={handleKeyPress}/>
             <input type="button" onClick={handleClick} value={"search"} />
         </div>
-        <h1 style={{color:"black"}}>{initState}</h1>
         <Tree arr={values}/>
         </div>
     )
